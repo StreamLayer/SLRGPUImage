3aa0eb2080aef767341d6ca70b6d95db27fd435f
bimawa <bimawa@gmail.com>
2022-06-30 13:16:09 +0500

    fix(spm): compiled for ios metallib

--- compileMetals.sh
+++ compileMetals.sh
@@ -3,7 +3,7 @@
 cd ./Sources/GPUImage/Metal
 
 for i in *.metal; do
-   xcrun -sdk macosx metal -c $i -o $(basename -s .metal $i).air
+   xcrun -sdk iphoneos metal -c $i -o $(basename -s .metal $i).air
 done
 
-xcrun -sdk macosx metallib *.air -o ../Resources/default.metallib
+xcrun -sdk iphoneos metallib *.air -o ../Resources/default.metallib
