e2f71f972b2281c4428f81336c9a4f0b798570db
bimawa <bimawa@gmail.com>
2022-08-09 12:55:30 +0500

    fix(script): update compile metal shaders

---
+++ Sources/GPUImageiOS/ DumpFile.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maxim Bunkov on 09.08.2022.
+//
+
+import Foundation
--- compileMetals.sh
+++ compileMetals.sh
@@ -6,4 +6,5 @@ for i in *.metal; do
    xcrun -sdk iphoneos metal -c $i -o $(basename -s .metal $i).air
 done
 
-xcrun -sdk iphoneos metallib *.air -o ../Resources/default.metallib
+xcrun -sdk iphoneos metallib *.air -o ../Resources/iOS/default.metallib
+xcrun -sdk iphonesimulator metallib *.air -o ../Resources/iOSSimulator/default.metallib
